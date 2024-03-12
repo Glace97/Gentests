ec1725fac6e5b02564cb0c87675445ff39caa56b
Glacier Ali <glacier.ali@gmail.com>
2024-03-12 17:39:44 +0100

    Remove private and protected funcs + func block extraction

--- java_parser.py
+++ java_parser.py
@@ -8,9 +8,65 @@ static_fun_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+\w+\s+\w+\s*\((?:.*?)\)
 # 1. fix edgecase if a function occurs within class variable
 # 2. remove private functions from metadata
 
+def read_function_block(java_code, start_pos):
+    brace_count = 0
+    pos = start_pos
+    while pos < len(java_code):
+        if java_code[pos] == '{':
+            brace_count += 1
+        elif java_code[pos] == '}':
+            brace_count -= 1
+            if brace_count == 0:
+                return pos
+        pos += 1
+    return -1  # If the end of the function is not found
+
+
+def remove_private_funcs(file):
+    # Read metadata
+    with open(file, "r") as input_file:
+        meta_data = input_file.read()
+    
+    # Patterns
+    # TODO: add patterns wherre there is no docstring prior
+    docstring_protected_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*protected\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+    docstring_protected_static_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*protected\s+static\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+    docstring_private_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*private\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+    docstring_private_static_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*private\s+static\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+    
+    patterns = [docstring_protected_pattern, docstring_protected_static_pattern, docstring_private_pattern, docstring_private_static_pattern]
+    
+    # Loop through metadata and remove private functions
+    while True:
+        # Flag to check if any match was found
+        match_found = False
+
+        for pattern in patterns:
+            match = re.search(pattern, meta_data, re.DOTALL)
+            if match:
+                # Match found, read function block
+                start_pos = match.start()
+                end_pos = read_function_block(meta_data, start_pos)
+
+                if end_pos != -1:
+                    # Remove the function block from metadata
+                    meta_data = meta_data[:start_pos] + meta_data[end_pos + 1:]
+                    match_found = True
+                    break 
+
+        # If no match was found, break the loop
+        if not match_found:
+            break
+
+    # Write the modified metadata back to the file
+    with open('trimmed_metadata.txt', "w") as output_file:
+        output_file.write(meta_data)
+
+
 
 
 def parse_metadata(): 
+    #TODO: add pattern to the variable names
     doc_string_fun = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+\w+\s+\w+\s*\((?:.*?)\)'
     doc_string_static_fun = r'/\*\*(?:(?!\*/).)*?\*/\s*(?:public\s+static\s+)?(?:\w+\s+)?\w+\s+\w+\s*\((?:.*?)\)'
 
@@ -45,13 +101,8 @@ def parse_metadata():
             output_file.write(content_before_match)
 
 
-#'''How to keep track of function scope'''
-# if pattern contains { open_brackets = 1
-# else read (next) line and open_brackets = 1
-# while openbrackest > 0
-# for each line
-#   if contains {, open_brackets += 1
-# print to output file
-# print closing bracket
+def main():
+    remove_private_funcs('metadata.txt')
 
-
+if __name__ == "__main__":
+    main()
