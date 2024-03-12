07090bab119b2fa969ffad685435783560d0b7a6
Glacier Ali <glacier.ali@gmail.com>
2024-03-12 17:15:52 +0100

    Add private and protected patterns with examples for later unit testing

--- tests/test_java_parser.py
+++ tests/test_java_parser.py
@@ -2,6 +2,10 @@ import re
 
 public_fun = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+\w+\s+\w+\s*\((?:.*?)\)'
 public_static_fun = r'/\*\*(?:(?!\*/).)*?\*/\s*(?:public\s+static\s+)?(?:\w+\s+)?\w+\s+\w+\s*\((?:.*?)\)'
+
+
+# PUBLIC FUNCTIONS WITHOUT DOCSTRINGS
+
 fun_return_val = """
 public returnType functionName(dataType arg1, dataType arg2)
 """
@@ -20,24 +24,8 @@ public static returnType functionName(dataType arg1, dataType arg2)
 {
 """
 
-docstring = """
-/**
- * This is a multi-line comment.
- * Another line.
- */
-public returnType functionName(dataType arg1, dataType arg2) {
-"""
 
-larger_docstring = """
-/**
- * This is a multi-line comment.
- * Another line.
- * And another one.
- */
-public static returnType functionName(dataType arg1, dataType arg2) {
-"""
-
-
+# DOCSTRINGS PREPENDED FUNCTION
 docstring = """
 /**
  * This is a multi-line comment.
@@ -62,5 +50,47 @@ docstring_class = """
 public class AnnotationUtils {
 """
 
+
+# PRIVATE AND PROTECTED FUNCTIONS WITH DOCSTRING
+
+docstring_private_static = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ */
+private static returnType functionName(dataType arg1, dataType arg2) {
+"""
+docstring_private_static_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*private\s+static\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+
+docstring_private = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ */
+private returnType functionName(dataType arg1, dataType arg2) {
+"""
+docstring_private_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*private\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+
+# PRIVATE AND PROTECTED FUNCTIONS WITH DOCSTRING
+
+docstring_protected_static = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ */
+protected static returnType functionName(dataType arg1, dataType arg2) {
+"""
+docstring_protected_static_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*protected\s+static\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+
+docstring_protected = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ */
+protected returnType functionName(dataType arg1, dataType arg2) {
+"""
+docstring_protected_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*protected\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
+
 # Find the first match for either pattern in the Java code
-# match = re.findall(public_static_fun, docstring_class, re.DOTALL)
+match = re.findall(docstring_protected_pattern, docstring_protected, re.DOTALL)
+print(match)
