04581cba1820272954fc85b202a708564198d76c
Glacier Ali <glacier.ali@gmail.com>
2024-03-14 11:00:20 +0100

    wip: regex pattern too general

--- tests/test_java_parser.py
+++ tests/test_java_parser.py
@@ -6,32 +6,45 @@ public_static_fun = r'/\*\*(?:(?!\*/).)*?\*/\s*(?:public\s+static\s+)?(?:\w+\s+)
 
 # PUBLIC FUNCTIONS WITHOUT DOCSTRINGS
 
-fun_return_val = """
+public_fun = """
 public returnType functionName(dataType arg1, dataType arg2)
 """
 
-fun_opening_bracket = """
-public returnType functionName(dataType arg1, dataType arg2) {
-"""
-
-fun_opening_bracket_newline = """
-public returnType functionName(dataType arg1, dataType arg2) 
-{
-"""
-
-fun_static_return_val = """
+public_static= """
 public static returnType functionName(dataType arg1, dataType arg2) 
 {
 """
 
+awful_returnval = """
+    public List<ActionResult<Map<String, long>>> functionName(dataType arg1, dataType arg2) 
+    { 
+"""
+public_fun_array_returnval = """
+public returnType[] functionName(dataType arg1, dataType arg2)
+"""
+
+# PRIVATE FUNCTIONS WITHOUT DOCSTRINGS
+
+priv_fun = """
+private returnType functionName(dataType arg1, dataType arg2)
+"""
+
+# PROTECTED FUNCTIONS WITHOUT DOCSTRINGS
+prot_fun_ = """
+protected returnType functionName(dataType arg1, dataType arg2)
+"""
+prot_fun_return_val_arr = """
+protected returnType[] functionName(dataType arg1, dataType arg2)
+"""
+
 
 # DOCSTRINGS PREPENDED FUNCTION
-docstring = """
+public_docstring = """
 /**
  * This is a multi-line comment.
  * Another line.
  */
-public Task<ActionResult<Dictionary<string, long>>> functionName(dataType arg1, dataType arg2) {
+public returnValue functionName(dataType arg1, dataType arg2) {
 """
 #docstring_fun_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+\w+\s+\w+\s*\((?:.*?)\)'
 #docstring_fun_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+([\w\[\]]+)\s+(\w+)\s*\((?:.*?)\)'
@@ -41,6 +54,15 @@ public Task<ActionResult<Dictionary<string, long>>> functionName(dataType arg1, 
 #docstring_fun_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+([^()\s]+(?:\s*<[^>]*>)?)\s+(\w+)\s*\((?:.*?)\)'
 docstring_fun_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*public\s+([^()\s]+(?:\s*<[^>]*>)?)\s+(\w+)\s*\((?:.*?)\)'
 
+
+public_docstring_awful_returnvalue = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ * Another lineee.
+ */
+public List<ActionResult<Map<String, long>>> functionName(dataType arg1, dataType arg2) {
+"""
 
 docstring_static = """
 /**
@@ -104,6 +126,25 @@ protected returnType functionName(dataType arg1, dataType arg2, final dataType a
 docstring_protected_pattern = r'/\*\*(?:(?!\*/).)*?\*/\s*protected\s+(\w+)\s+(\w+)\s*\((.*?)\)\s*\{'
 
 
+
+# SHOULD NEVER MATCH
+
+constructor = """
+/**
+ * This is a multi-line comment.
+ * Another line.
+ *fnfea
+ */
+public className(dataType arg1, dataType arg2, final dataType arg3) {
+"""
+
+
+match_all_pattern = r'(public|protected|private|static|\s)+[\w\<\>\[\]]+\s+(\w+)\s*\([^)]*\)\s*({|[^;])'
+
+
+
 # Find the first match for either pattern in the Java code
-match = re.findall(docstring_fun_pattern, docstring, re.DOTALL)
+#match = re.findall(docstring_match_all_pattern, docstring_protected, re.DOTALL)
+match = re.findall(match_all_pattern, constructor)
+
 print(match)
