b147513f71c4fddddaa38d0ec69f83a1b24d7508
Glacier Ali <glacier.ali@gmail.com>
2024-03-25 09:20:04 +0100

    Separate extraction of methods from context

--- Parser/src/main/java/MethodExtractor.java
+++ Parser/src/main/java/MethodExtractor.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -10,31 +9,24 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-
 /**
  * Extracts information from Javafiles.
  * Imports, packages, methods, class variables, enums.
  */
-public class Extractor extends JavaParserBaseListener {
+public class MethodExtractor extends JavaParserBaseListener {
     String[] methodNamesToMatch;
     File currentJavaFile;
     File outputDir;
     List<Interval> intervals;
+    ArrayList<String> metaData;
     CharStream input;
 
-    public Extractor(String[] methodNames, String outputDir) {
+    public MethodExtractor(String[] methodNames, String outputDir) {
         this.methodNamesToMatch = methodNames;
         this.outputDir = new File(outputDir);
         this.outputDir.mkdirs();
     }
 
-    @Override public void enterImportDeclaration(JavaParser.ImportDeclarationContext ctx) {
-        int a = ctx.start.getStartIndex();
-        int b = ctx.stop.getStopIndex();
-        Interval interval = new Interval(a,b);
-        intervals.add(interval);
-    }
-    
     @Override public void enterMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
         JavaParser.IdentifierContext identifier = ctx.identifier();
         if (identifier!=null) {
@@ -61,11 +53,10 @@ public class Extractor extends JavaParserBaseListener {
         }
     }
 
-
     private void parseFile(File child) {
         try {
             currentJavaFile = child;
-            intervals = new ArrayList<Interval>();
+            intervals = new ArrayList<>();
             input = new ANTLRFileStream(child.getPath());
             JavaLexer lexer = new JavaLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -73,7 +64,7 @@ public class Extractor extends JavaParserBaseListener {
             ParserRuleContext tree = parser.compilationUnit();
             ParseTreeWalker walker = new ParseTreeWalker();
             walker.walk(this, tree);
-            if (intervals.size()>0) {
+            if (!intervals.isEmpty()) {
                 writeOutputFile();
             }
         } catch (IOException e) {
@@ -84,15 +75,17 @@ public class Extractor extends JavaParserBaseListener {
 
     private void writeOutputFile() {
         String nameWithExtension =  this.currentJavaFile.getName();
-        String outFileName = nameWithExtension.split("\\.")[0] + "_methods";
-        File outFile = new File(this.outputDir, outFileName);
+        String methodsToTest = nameWithExtension.split("\\.")[0] + "_methods";
+        File outFile = new File(this.outputDir, methodsToTest);
         try {
+            // Write methods to test
             FileWriter fw = new FileWriter(outFile);
             for (Interval interval:this.intervals) {
                 fw.write(input.getText(interval));
                 fw.write("\n\n");
             }
             fw.close();
+
         } catch (Exception e) {
             System.err.println("Could not write output file " + outFile);
             e.printStackTrace();
@@ -103,7 +96,7 @@ public class Extractor extends JavaParserBaseListener {
         String[] methodNames = {"calcLuhn", "validateLuhn", "someMethod", "someOtherMethod"};
         File luhnCalculator = new File("/Users/glacierali/repos/MEX/poc/Parser/src/main/java/testclasses");
         String outputDir = "/Users/glacierali/repos/MEX/poc/Parser/src/main/java/output";
-        Extractor extractor = new Extractor(methodNames, outputDir);
+        MethodExtractor extractor = new MethodExtractor(methodNames, outputDir);
         extractor.walkDirectory(luhnCalculator);
     }
 
