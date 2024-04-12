4492c7428871d3d2828ec901597e34109777aa08
Glacier Ali <glacier.ali@gmail.com>
2024-04-12 15:24:14 +0200

    Also include testfiles which does not throw errors

--- parse_mvn_test_report.py
+++ parse_mvn_test_report.py
@@ -3,13 +3,20 @@ import csv
 
 def parse_maven_report(report):
     data = []
-    pattern = r'\[INFO\] Running (.+)\n\[ERROR\] Tests run: (\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+), Time elapsed: (\d+\.\d+) s <<< FAILURE! -- in (.+)'
+    pattern_with_errors = r'\[INFO\] Running (.+)\n\[ERROR\] Tests run: (\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+), Time elapsed: (\d+\.\d+) s <<< FAILURE! -- in (.+)'
+    pattern_without_errors = r'\[INFO\] Running (.+)\n\[INFO\] Tests run: (\d+), Failures: (\d+), Errors: (\d+), Skipped: (\d+), Time elapsed: (\d+\.\d+) s -- in (.+)'
 
-    matches = re.findall(pattern, report)
-    for match in matches:
-        #print('match', match)
+    matches_with_errors = re.findall(pattern_with_errors, report)
+    matches_without_errors = re.findall(pattern_without_errors, report)
+
+    for match in matches_with_errors:
+        #print(match)
         class_name, num_tests, num_failures, num_errors, num_skipped, _, _ = match
         data.append((class_name, int(num_tests), int(num_failures), int(num_errors), int(num_skipped)))
+
+    for match in matches_without_errors:
+        class_name, num_tests, _, _, num_skipped, _, _ = match
+        data.append((class_name, int(num_tests), 0, 0, int(num_skipped)))
     
     return data
 
@@ -25,7 +32,7 @@ def main():
         report = file.read()
 
     parsed_data = parse_maven_report(report)
-    write_to_csv(parsed_data, 'maven_test_report.csv')
+    write_to_csv(parsed_data, 'test_summary.csv')
 
 if __name__ == "__main__":
     main()
