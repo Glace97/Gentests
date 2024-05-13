19429df4b147bf2ef381bcf87af8a4e3155944b3
Glacier Ali <glacier.ali@gmail.com>
2024-05-13 14:12:23 +0200

    Plot different kind of test failures

---
+++ test_evaluation/plot_test_failures.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Incorrect tests
+# Num of compiling tests: 1971
+'''
+data = {
+    'Test Suite': ['V1 GPT3.5-turbo-16k', 'V2 GPT3.5-turbo-16k', 'V2 GPT4'],
+    'AssertionFailedError': [184, 87, 96],
+    'Throws java.lang exception': [10, 11, 10],
+    'Mockito Exceptions': [0, 30, 6],
+    'Mockito cannot mock/spy because - final class': [0, 10, 10]
+}
+'''
+
+# Non compiling teests
+# Num of all tests: 2501
+#'''
+data = {
+    'Test Suite': ['V1 GPT3.5-turbo-16k', 'V2 GPT3.5-turbo-16k', 'V2 GPT4'],
+    'Cannot Resolve Method': [5,13, 11],  # Ambigious method call more common in cases of polymorphism
+    'Ambigious Method call': [5, 15, 13],
+    'Incompatible types: cannot be converted': [0, 8, 1],
+    'Mockito: Fatal error': [0, 3, 0],
+    'Method in class cannot be applied to given types': [1, 15, 1]
+}
+
+#'''
+
+# Convert the dictionary to a DataFrame
+df = pd.DataFrame(data)
+
+# Set the 'Test Suite' column as index
+df.set_index('Test Suite', inplace=True)
+
+# Plot a stacked bar chart
+ax = df.plot(kind='bar', stacked=True, figsize=(10, 7))
+
+# Rotate x-axis labels
+plt.xticks(rotation=0, ha='center')
+
+
+# Adding titles and labels
+plt.title('Non-compiling tests')
+plt.title('Non-compiling tests')
+
+plt.xlabel('Test Suite')
+#plt.ylabel('Number of Failures')
+
+# Show legend
+plt.legend(title='Error Types')
+
+# Display the plot
+plt.show()
