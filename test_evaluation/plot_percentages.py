1f342c5f7b3cf7edd4bc16d8ac8b62e5324836fa
Glace97 <glacier.ali@gmail.com>
2024-06-10 12:14:55 +0200

    Fix incorrect numbers on correct tests

--- test_evaluation/plot_percentages.py
+++ test_evaluation/plot_percentages.py
@@ -9,8 +9,8 @@ methods = ['Randoop', 'V1 GPT3.5-turbo-16k', 'Human-written']
 ratios = [1379/1416, 6235/9434, 4195/4195]  
 '''
 # Subset of the data compilation rate
-methods = ['V1 GPT3.5-turbo-16k', 'V2 GPT3.5-turbo-16k', 'V2 GPT4']
-ratios = [1971/2501, 1971/2162, 1673/1725]  # Actual ratios based on your values
+#methods = ['V1 GPT3.5-turbo-16k', 'V2 GPT3.5-turbo-16k', 'V2 GPT4']
+#ratios = [1971/2501, 1971/2162, 1673/1725]  # Actual ratios based on your values
 
 
 ### DATA FAILING/CORRECT TESTS ####
@@ -19,11 +19,11 @@ methods = ['Randoop', 'V1 GPT3.5-turbo-16k', 'Human-written']
 ratios = [0, 530/2501, 0]  # Actual ratios based on your values
 '''
 # Subset of the data pass/fail rate
-'''
+#'''
 methods = ['V1 GPT3.5-turbo-16k', 'V2 GPT3.5-turbo-16k', 'V2 GPT4']
 #ratios = [530/2501, 148/1971, 111/1673]  #Fail rate
-ratios = [1441/2501, 1971/2162, 1562/1673]
-'''
+ratios = [1774/1971, 1823/1971, 1562/1673]
+#'''
 
 
 # Creating the bar chart
@@ -31,13 +31,13 @@ plt.figure(figsize=(8, 4))  # Size of the figure
 plt.bar(methods, ratios, color=['#1f77b4', '#72b754', '#ef5050'])
 
 # Adding titles and labels
-#plt.title('Percentage of correct tests')
-plt.title('Percentage of Compiling Tests')
+plt.title('Percentage of correct tests')
+#plt.title('Percentage of Compiling Tests')
 
 plt.xlabel('Method')
 
-#plt.ylabel('Correct tests (%)')
-plt.ylabel('Compiling (%)')
+plt.ylabel('Correct tests (%)')
+#plt.ylabel('Compiling (%)')
 
 # Set the y-axis to display percentage
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
@@ -54,3 +54,8 @@ for i, ratio in enumerate(ratios):
 
 # Show the plot
 plt.show()
+
+# Clear cache
+plt.clf()
+plt.cla()
+plt.close()
